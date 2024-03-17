9b8ffd612fb132feff237bd8ab0a0427685eb998
Maxime Lebas <77737454+MokoyS@users.noreply.github.com>
2024-03-17 23:08:13 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+console.log("yo");
+gsap.from('.titre', {opacity: 0, delay:0.5, duration:8}) 
+gsap.from('h2', {opacity: 0, delay:1, duration:8}) 
+gsap.from('p', {opacity: 0, delay:1.5, duration:8}) 
+gsap.from('h3', {opacity: 0, delay:2, duration:8}) 
+
+
